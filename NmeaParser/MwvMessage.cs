ccfa1d7dff369aa5a5e28639a399028532bdee80
Kacper Bednarczyk <kbednarczyk@autocomp.com.pl>
2026-05-14 14:04:07 +0200

    Add MwvMessage class for parsing MWV NMEA messages

---
+++ NmeaParser/MwvMessage.cs
@@ -0,0 +1,43 @@
+using System.Globalization;
+using Autocomp.Nmea.Common;
+
+namespace NmeaParser;
+
+public class MwvMessage : BaseMessage
+{
+    public decimal WindAngle { get; }
+    public bool Reference { get; }
+    public decimal WindSpeed { get; }
+    public WindSpeedUnit Unit { get; }
+
+    public MwvMessage(string body) : base(body)
+    {
+        // Wind Angle
+        decimal windAngle = Convert.ToDecimal(Fields[0], CultureInfo.InvariantCulture);
+
+        // Reference
+        bool reference = false;
+
+        if (Fields[1].ToUpper() == "R")
+            reference = true;
+        else if (Fields[1].ToUpper() == "T")
+            reference = false;
+        else
+            throw new NotSupportedException($"Invalid reference: {Fields[1]}");
+
+        // Wind Speed
+        decimal windSpeed = Convert.ToDecimal(Fields[2], CultureInfo.InvariantCulture);
+
+        // Unit
+        string rawUnit = Fields[3].Substring(0, 1);
+        if (!Enum.TryParse(rawUnit, out WindSpeedUnit unit))
+            throw new NotSupportedException($"Invalid unit: {rawUnit}");
+
+        this.WindAngle = windAngle;
+        this.Reference = reference;
+        this.WindSpeed = windSpeed;
+        this.Unit = unit;
+    }
+
+    public enum WindSpeedUnit { K, M, N, S }
+};
