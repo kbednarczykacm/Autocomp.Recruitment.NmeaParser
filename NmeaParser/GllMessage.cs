9daf51b709825b4b6834d43b8199e6fad3939b6c
Kacper Bednarczyk <kbednarczyk@autocomp.com.pl>
2026-05-15 13:52:29 +0200

    Add error handling in GllMessage constructor

--- NmeaParser/GllMessage.cs
+++ NmeaParser/GllMessage.cs
@@ -13,38 +13,80 @@ public class GllMessage : BaseMessage
 
     public GllMessage(string body) : base(body)
     {
-        // Latitude
-        decimal rawLatitude = Convert.ToDecimal(Fields[0], CultureInfo.InvariantCulture);
-        byte latitudeDegrees = (byte)Math.Floor(rawLatitude / 100);
-        decimal latitudeMinutes = rawLatitude - latitudeDegrees * 100;
-        bool latitudeDirection = false;
+        // Check number of arguments
+        if (Fields.Length > 7)
+            throw new ArgumentOutOfRangeException($"Too many arguments: {Fields.Length}");
+        if (Fields.Length < 7)
+            throw new ArgumentOutOfRangeException($"Too few arguments: {Fields.Length}");
 
+        // Latitude
+        if (!Decimal.TryParse(Fields[0], CultureInfo.InvariantCulture, out decimal rawLatitude))
+            throw new ArgumentException($"Invalid latitude value: {Fields[0]}");
+        // Degrees
+        decimal rawLatitudeDegrees = Math.Floor(rawLatitude / 100);
+        if (rawLatitudeDegrees > 90)
+            throw new ArgumentOutOfRangeException($"Too high latitude degrees value: {rawLatitudeDegrees}");
+        if (rawLatitudeDegrees < 0)
+            throw new ArgumentOutOfRangeException($"Too low latitude degrees value: {rawLatitudeDegrees}");
+        byte latitudeDegrees = (byte)rawLatitudeDegrees;
+        // Minutes
+        decimal latitudeMinutes = rawLatitude - latitudeDegrees * 100;
+        if (latitudeMinutes >= 60)
+            throw new ArgumentOutOfRangeException($"Too high latitude minutes value: {latitudeMinutes}");
+        // Direction
+        bool latitudeDirection = false;
         if (Fields[1].ToUpper() == "N")
             latitudeDirection = true;
         else if (Fields[1].ToUpper() == "S")
             latitudeDirection = false;
         else
-            throw new NotSupportedException($"Invalid latitude direction: {Fields[1]}");
+            throw new ArgumentException($"Invalid latitude direction: {Fields[1]}");
 
         // Longitude
-        decimal rawLongitude = Convert.ToDecimal(Fields[2], CultureInfo.InvariantCulture);
-        byte longitudeDegrees = (byte)Math.Floor(rawLongitude / 100);
+        if (!Decimal.TryParse(Fields[2], CultureInfo.InvariantCulture, out decimal rawLongitude))
+            throw new ArgumentException($"Invalid latitude value: {Fields[2]}");
+        // Degrees
+        decimal rawLongitudeDegrees = Math.Floor(rawLongitude / 100);
+        if (rawLongitudeDegrees > 180)
+            throw new ArgumentOutOfRangeException($"Too high longitude degrees value: {rawLongitudeDegrees}");
+        if (rawLongitudeDegrees < 0)
+            throw new ArgumentOutOfRangeException($"Too low longitude degrees value: {rawLongitudeDegrees}");
+        byte longitudeDegrees = (byte)rawLongitudeDegrees;
+        // Minutes
         decimal longitudeMinutes = rawLongitude - longitudeDegrees * 100;
+        if (longitudeMinutes >= 60)
+            throw new ArgumentOutOfRangeException($"Too high longitude minutes value: {longitudeMinutes}");
+        // Direction
         bool longitudeDirection = false;
-
         if (Fields[3].ToUpper() == "W")
             longitudeDirection = true;
         else if (Fields[3].ToUpper() == "E")
             longitudeDirection = false;
         else
-            throw new NotSupportedException($"Invalid longitude direction: {Fields[3]}");
+            throw new ArgumentException($"Invalid longitude direction: {Fields[3]}");
 
         // Time
-        decimal rawTime = Convert.ToDecimal(Fields[4], CultureInfo.InvariantCulture);
-        int hours = (int)Math.Floor(rawTime / 10000);
-        int minutes = (int)Math.Floor((rawTime - hours * 10000) / 100);
-        int seconds = (int)Math.Floor(rawTime - hours * 10000 - minutes * 100);
-        int milliseconds = (int)(rawTime - hours * 10000 - minutes * 100 - seconds) * 1000;
+        if (!Decimal.TryParse(Fields[4], CultureInfo.InvariantCulture, out decimal rawTime))
+            throw new ArgumentException($"Invalid time value: {Fields[4]}");
+        // Hours
+        decimal rawHours = Math.Floor(rawTime / 10000);
+        if (rawHours > 23)
+            throw new ArgumentOutOfRangeException($"Too high hours value: {rawHours}");
+        if (rawHours < 0)
+            throw new ArgumentOutOfRangeException($"Too low hours value: {rawHours}");
+        int hours = (int)rawHours;
+        // Minutes
+        decimal rawMinutes = Math.Floor((rawTime - hours * 10000) / 100);
+        if (rawMinutes > 59)
+            throw new ArgumentOutOfRangeException($"Too high minutes value: {rawMinutes}");
+        int minutes = (int)rawMinutes;
+        // Seconds
+        decimal rawSeconds = Math.Floor(rawTime - hours * 10000 - minutes * 100);
+        if (rawSeconds > 59)
+            throw new ArgumentOutOfRangeException($"Too high seconds value: {rawSeconds}");
+        int seconds = (int)rawSeconds;
+        // Milliseconds
+        int milliseconds = (int)((rawTime - hours * 10000 - minutes * 100 - seconds) * 1000);
 
         // Status
         bool status;
@@ -53,13 +95,14 @@ public class GllMessage : BaseMessage
         else if (Fields[5].ToUpper() == "V")
             status = false;
         else
-            throw new NotSupportedException($"Invalid status descriptor: {Fields[5]}");
+            throw new ArgumentException($"Invalid status descriptor: {Fields[5]}");
 
         // Mode
         string rawMode = Fields[6].Substring(0, 1);
         if (!Enum.TryParse(rawMode, out ModeIndicator mode))
-            throw new NotSupportedException($"Invalid mode indicator: {rawMode}");
+            throw new ArgumentException($"Invalid mode indicator: {rawMode}");
 
+        // Assignement
         this.Latitude = new Coordinate(latitudeDegrees, latitudeMinutes, latitudeDirection);
         this.Longitude = new Coordinate(longitudeDegrees, longitudeMinutes, longitudeDirection);
         this.Time = new TimeOnly(hours, minutes, seconds, milliseconds);
