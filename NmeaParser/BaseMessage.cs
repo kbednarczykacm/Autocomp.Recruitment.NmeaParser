90ae2357cf3f8f08b59bf0fe99f5bdef0e2d289d
Kacper Bednarczyk <kbednarczyk@autocomp.com.pl>
2026-05-18 12:17:34 +0200

    Replace hardcoded literals with NmeaFormat

--- NmeaParser/BaseMessage.cs
+++ NmeaParser/BaseMessage.cs
@@ -16,16 +16,21 @@ public abstract class BaseMessage : NmeaMessage
     {
         TalkerId = Header.Substring(1, 2);
         MessageType = Header.Substring(3);
-        Checksum = Convert.FromHexString(Fields[Fields.Length - 1].Split("*")[1].Substring(0, 2))[0];
-        Message = body.Split("$")[1].Split("*")[0];
+        Checksum = Convert.FromHexString(Fields[Fields.Length - 1].Split(Format.Suffix)[1].Substring(0, 2))[0];
+        Message = body.Split(Format.Prefix)[1].Split(Format.Suffix)[0];
     }
 
     public static BaseMessage CreateMessage(string rawMessage)
     {
-        if (string.IsNullOrWhiteSpace(rawMessage) || !rawMessage.StartsWith("$"))
+        return CreateMessage(rawMessage, NmeaFormat.Default);
+    }
+
+    public static BaseMessage CreateMessage(string rawMessage, NmeaFormat format)
+    {
+        if (string.IsNullOrWhiteSpace(rawMessage) || !rawMessage.StartsWith(format.Prefix))
             throw new ArgumentException("Invalid NMEA message format");
 
-        string[] fields = rawMessage.Split(",");
+        string[] fields = rawMessage.Split(format.Separator);
         string header = fields[0];
 
         if (header.Length < 6)
